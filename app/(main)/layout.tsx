3837e85a9bb1645e0bc8ddee124354d39ade66ac
Algorithmicmasonry <emmanuelandalgorithms@gmail.com>
2025-05-28 15:10:51 +0100

    updated metadata

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,8 +1,43 @@
 import { ContactUs, Footer, Navbar } from "@/components/landing-page";
 import { Toaster } from "@/components/ui/toaster";
-import { defaultContent } from "@/constants";
+import { appName, defaultContent } from "@/constants";
 import { fetchContentData } from "@/server actions/content.actions";
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: appName,
+  description:
+    "Discover high quality reliable new and foreign used vehicles at great prices",
+  metadataBase: new URL("https://car-dealership-one-seven.vercel.app"),
+
+  openGraph: {
+    title: "Find the Best Premium Cars Online",
+    description:
+      "Discover high quality reliable new and foreign used vehicles at great prices",
+    url: "https://car-dealership-one-seven.vercel.app",
+    siteName: appName,
+    locale: "en_US",
+    type: "website",
+    images: [
+      {
+        url: "/car-dealership.webp",
+        width: 1200,
+        height: 630,
+        alt: "Durie Autos vehicle showcase",
+      },
+    ],
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Find the Best Premium Cars Online",
+    description:
+      "Discover high quality reliable new and foreign used vehicles at great prices",
+    creator: "@yourtwitterhandle",
+    images: ["https://car-dealership-one-seven.vercel.app/car-dealership.webp"],
+  },
+};
 
 
 
